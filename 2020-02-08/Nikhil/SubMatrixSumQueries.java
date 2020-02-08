a4af00891b6fac1fc4babefa7fee22aa84299939
Nikhil <nikhil.kothi@medlife.com>
2020-02-08 23:05:35 +0530

    SubMatrixSumQueries problem

---
+++ 2020-02-08/Nikhil/SubMatrixSumQueries.java
@@ -0,0 +1,44 @@
+package interviewbit;
+
+/**
+ * @Author: Nikhil Reddy (nikoth)
+ * @Date: 08/02/20, Sat
+ **/
+public class SubMatrixSumQueries {
+
+    public int[] solve(int[][] A, int[] B, int[] C, int[] D, int[] E) {
+
+        final int modulo = 1000000007;
+
+        for(int index = 0; index<A.length; index++)
+            for(int walker = 0; walker<A[index].length; walker++)
+                A[index][walker] = A[index][walker]%modulo;
+
+        for(int index = 1; index<A.length; index++)
+            for(int walker = 0; walker< A[index].length; walker++)
+                A[index][walker] = (A[index][walker]+A[index-1][walker])%modulo;
+
+        for(int index = 0; index<A.length; index++)
+            for(int walker = 1; walker< A[index].length; walker++)
+                A[index][walker] = (A[index][walker]+A[index][walker-1])%modulo;
+
+        int[] sum = new int[B.length];
+
+        for(int index = 0; index<B.length; index++){
+            int x1 = B[index] - 1;
+            int y1 = C[index] - 1;
+            int x2 = D[index] - 1;
+            int y2 = E[index] - 1;
+
+            if(x1 == 0 && y1 == 0)
+                sum[index] = A[x2][y2];
+            else if(x1 ==0)
+                sum[index] = A[x2][y2] - A[x2][y1-1];
+            else if(y1 == 0)
+                sum[index] = A[x2][y2] - A[x1-1][y2];
+            else
+                sum[index] = (A[x2][y2] + A[x1-1][y1-1])%modulo - A[x1-1][y2] - A[x2][y1-1];
+        }
+        return sum;
+    }
+}
